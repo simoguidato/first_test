0db471cc035512ea0a05f46b67656838151482db
simoguidato <simoguidato@libero.it>
2025-03-21 10:30:29 +0100

    Primo commit

---
+++ src/main/java/com/mio/progetto/App.java
@@ -0,0 +1,13 @@
+package com.mio.progetto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/mio/progetto/Main.java
@@ -0,0 +1,8 @@
+package com.mio.progetto;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
+
